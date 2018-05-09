b0d6a1bedf7ac964f28193df05ee2a172f55cb60
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-09 17:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(n) {
 }
 
 function removeLastKitten() {
-  return [...kittens, n];
+  return kittens.slice(0, kittens.length - 1);
 }
 function removeFirstKitten() {
   return kittens.slice(1);
